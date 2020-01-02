8dcf55ffa39ed31af7d0902cf07dae23563934d7
Dheeraj <mr.dheeraj000@gmail.com>
2020-01-02 20:10:01 +0530

    push

---
+++ app/src/main/java/com/corp/camerax/MainActivity.kt
@@ -0,0 +1,173 @@
+package com.corp.camerax
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import java.io.File
+import java.nio.ByteBuffer
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+class MainActivity : AppCompatActivity() ,LifecycleOwner
+{
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewFinder = findViewById(R.id.view_finder)
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+            updateTransform()
+        }
+
+
+    }
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().apply {
+            setTargetResolution(Size(1080, 920))
+        }.build()
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+
+
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+            updateTransform()
+
+        }
+        val imageCaptureConfig = ImageCaptureConfig.Builder()
+            .apply {
+
+                setCaptureMode(ImageCapture.CaptureMode.MIN_LATENCY)
+            }.build()
+        val imageCapture = ImageCapture(imageCaptureConfig)
+        findViewById<ImageButton>(R.id.capture_button).setOnClickListener {
+            val file = File(externalMediaDirs.first(),
+                "${System.currentTimeMillis()}.jpg")
+
+            imageCapture.takePicture(file, executor,
+                object : ImageCapture.OnImageSavedListener {
+                    override fun onError(
+                        imageCaptureError: ImageCapture.ImageCaptureError,
+                        message: String,
+                        exc: Throwable?
+                    ) {
+                        val msg = "Photo capture failed: $message"
+                        Log.e("CameraXApp", msg, exc)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onImageSaved(file: File) {
+                        val msg = "Photo capture succeeded: ${file.absolutePath}"
+                        Log.d("CameraXApp", msg)
+                        viewFinder.post {
+                            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                })
+        }
+        val analyzerConfig = ImageAnalysisConfig.Builder().apply {
+
+            setImageReaderMode(
+                ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
+        }.build()
+
+
+        val analyzerUseCase = ImageAnalysis(analyzerConfig).apply {
+            setAnalyzer(executor, LuminosityAnalyzer())
+        }
+
+        CameraX.bindToLifecycle(
+            this, preview, imageCapture, analyzerUseCase)
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+        viewFinder.setTransform(matrix)
+
+
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+    inner private class LuminosityAnalyzer : ImageAnalysis.Analyzer {
+        private var lastAnalyzedTimestamp = 0L
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()
+            val data = ByteArray(remaining())
+            get(data)
+            return data
+        }
+
+        override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+            val currentTimestamp = System.currentTimeMillis()
+
+            if (currentTimestamp - lastAnalyzedTimestamp >=
+                TimeUnit.SECONDS.toMillis(1)) {
+
+                val buffer = image.planes[0].buffer
+                val data = buffer.toByteArray()
+
+                val pixels = data.map { it.toInt() and 0xFF }
+
+                val luma = pixels.average()
+
+                Log.d("CameraXApp", "Average luminosity: $luma")
+
+                lastAnalyzedTimestamp = currentTimestamp
+            }
+        }
+    }
+}
